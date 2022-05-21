788d3fd96781aff63097566395ffe7b649d4b906
Francesco <fr.ilario@gmail.com>
2022-05-21 15:37:56 +0200

    Update thctl

--- cmd/thctl/main.go
+++ cmd/thctl/main.go
@@ -21,42 +21,50 @@ func main() {
 }
 
 func run() error {
+	b, err := readSensor()
+	if err != nil {
+		return fmt.Errorf("error reading data from sensor: %w", err)
+	}
+	log.Printf("Read data from sensor: '%s'\n", b)
+
+	m, t := string(b), "garden/temphum"
+	if err := sendMessageToIoT(t, m); err != nil {
+		return fmt.Errorf("error sending message to IoT Hub: %w", err)
+	}
+	log.Printf("Message '%s' sent to topic '%s'", m, t)
+
+	return nil
+}
+
+func readSensor() ([]byte, error) {
 	conn, err := grpc.Dial(SockAddr, grpc.WithInsecure())
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	cli := thprotos.NewTempHumSvcClient(conn)
 	ctx := context.Background()
 	m, err := cli.ReadTempHum(ctx, &thprotos.ReadTempHumRequest{})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	b, err := json.Marshal(m)
 	if err != nil {
-		return fmt.Errorf("error marshaling ReadTempHumResponse (%v) as JSON: %w", m, err)
+		return nil, fmt.Errorf("error marshaling ReadTempHumResponse (%v) as JSON: %w", m, err)
 	}
-	fmt.Printf("%s\n", b)
-
-	if err := sendMessageToIoT(string(b)); err != nil {
-		return fmt.Errorf("error sending message to IoT Hub: %w", err)
-	}
-	return nil
+	return b, nil
 }
 
-func sendMessageToIoT(msg string) error {
+func sendMessageToIoT(topic, msg string) error {
 	cfg, err := iothubmqtt.BuildConfigFromEnv("IOT_")
 	if err != nil {
 		return fmt.Errorf("error building configuration for MQTT Client: %w", err)
 	}
 
 	ihc := iothubmqtt.NewMQTTClient(cfg)
-	t := "garden/temphum"
-	if err := ihc.Publish(t, msg); err != nil {
+	if err := ihc.Publish(topic, msg); err != nil {
 		return err
 	}
-	log.Printf("Message '%s' sent to topic '%s'", msg, t)
 	return nil
-
 }
