657f6aefc28d708070f1843842318dea5acc01c3
Francesco <fr.ilario@gmail.com>
2022-05-22 21:25:19 +0200

    Add IoTHubBroker HTTP Server

---
+++ cmd/iothubbroker/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/filariow/thsock/pkg/iothubmqtt"
+)
+
+const SockAddr = "unix:/tmp/th.socket"
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	log.Println("Configuring HTTP server")
+	http.HandleFunc("event", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			m := fmt.Sprintf("Expected method %s, found: %s", http.MethodPost, r.Method)
+			log.Println(m)
+
+			w.Write([]byte(m))
+			w.WriteHeader(405)
+			return
+		}
+
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			m := fmt.Sprintf("error reading request body: %s", err)
+			log.Println(m)
+
+			w.Write([]byte(m))
+			w.WriteHeader(400)
+			return
+		}
+
+		if err := sendMessageToIoT(string(b)); err != nil {
+			m := fmt.Sprintf("error sending message to IoT Hub: %s", err)
+			log.Println(m)
+
+			w.Write([]byte(m))
+			w.WriteHeader(400)
+			return
+		}
+
+		w.WriteHeader(200)
+	})
+
+	p := "8080"
+	log.Printf("Serving on port: %s", p)
+	return http.ListenAndServe(":"+p, nil)
+}
+
+func sendMessageToIoT(msg string) error {
+	cfg, err := iothubmqtt.BuildConfigFromEnv("IOT_")
+	if err != nil {
+		return fmt.Errorf("error building configuration for MQTT Client: %w", err)
+	}
+
+	t := fmt.Sprintf("devices/%s/messages/events/", cfg.ClientID)
+	ihc := iothubmqtt.NewMQTTClient(cfg)
+	if err := ihc.Publish(t, msg); err != nil {
+		return err
+	}
+
+	log.Printf("Message '%s' sent to topic '%s'", msg, t)
+	return nil
+}
