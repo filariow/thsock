d15b5f87200f050fc7f2a4442231074340fd5383
Francesco <fr.ilario@gmail.com>
2022-06-29 03:06:24 +0200

    Add weather app for cloud k8s

---
+++ cmd/edge/thsampler/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"context"
+
+	"github.com/filariow/thsock/pkg/ihbclient"
+	"github.com/filariow/thsock/pkg/thprotos"
+	"google.golang.org/grpc"
+)
+
+const SockAddr = "unix:/tmp/th.socket"
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	a := os.Getenv("IOT_ADDRESS")
+	c, err := ihbclient.NewClient(a)
+	if err != nil {
+		return fmt.Errorf("error creating client for IoT Hub Broker: %w", err)
+	}
+
+	b, err := readSensor()
+	if err != nil {
+		return fmt.Errorf("error reading data from sensor: %w", err)
+	}
+	log.Printf("Read data from sensor: '%s'\n", b)
+
+	ctx := context.Background()
+	if err := sendMessageToIoT(ctx, c, b); err != nil {
+		return fmt.Errorf("error sending message to IoT Hub: %w", err)
+	}
+
+	return nil
+}
+
+func readSensor() ([]byte, error) {
+	conn, err := grpc.Dial(SockAddr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	cli := thprotos.NewTempHumSvcClient(conn)
+	ctx := context.Background()
+	m, err := cli.ReadTempHum(ctx, &thprotos.ReadTempHumRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling ReadTempHumResponse (%v) as JSON: %w", m, err)
+	}
+	return b, nil
+}
+
+func sendMessageToIoT(ctx context.Context, c ihbclient.IHBClient, data []byte) error {
+	a := c.Address()
+	log.Printf("Sending data to IoT Hub broker at '%s'", a)
+
+	r, err := c.SendEvent(ctx, data)
+	if err != nil {
+		return err
+	}
+	if r.StatusCode > 299 {
+		return fmt.Errorf("Error sending message to IoTHub Broker at '%s'. Response: %v", a, *r)
+	}
+
+	log.Printf("Data sent, successful response: %v", *r)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -10,12 +10,29 @@ require (
 )
 
 require (
+	github.com/Shopify/sarama v1.34.1 // indirect
 	github.com/d2r2/go-shell v0.0.0-20191113051817-7664ea33645f // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.15.6 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
 )
